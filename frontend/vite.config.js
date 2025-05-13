9ada18b48e419cffc41fb3a2942d096281ad2265
Tatyana N <mairon.angband@gmail.com>
2025-05-13 20:49:21 +0300

    fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,19 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5002,
+    port: 5001,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
       '/socket.io': {
         target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
-      },
-      '/': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
       },
     }
   }
