f27cc2cb191fc33d18a2eccdd94f52d69d9dcbf3
Tatyana N <mairon.angband@gmail.com>
2025-05-15 23:02:38 +0300

    formik

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -11,35 +11,48 @@ const Login = () => {
     },
   });
   return (
-    <div>
-      <h1>Hexlet Chat</h1>
-      <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
+    <Formik
+      initialValues={{
+        email: "",
+        password: ""
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, isSubmitting }) => (
+        <div>
+          <h1>Hexlet Chat</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
 
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-    </div>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+        </div>
+      )}
+    </Formik>
   );
 };
 
