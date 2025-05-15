803c4b265dfb8f57cd195573d70fa32ec4d92c8a
Tatyana N <mairon.angband@gmail.com>
2025-05-15 22:51:39 +0300

    formik

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -4,38 +4,38 @@ import { Formik } from 'formik';
 const Login = () => {
   return (
     <div>
-    <h1>Hexlet Chat</h1>
-    <Formik>
-        <form>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          <label htmlFor="password">Password</label>
+      <h1>Hexlet Chat</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          console.log('Form Submitted', values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label htmlFor="email">E-mail</label>
+            <Field
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+            />
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
 
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-       </form>
-
-    </Formik>
-  </div>
-  )
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default Login;
