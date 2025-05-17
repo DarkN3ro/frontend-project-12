a1b77d27b46483eb226dab3df295c934aa3ef727
Tatyana N <mairon.angband@gmail.com>
2025-05-17 16:14:14 +0300

    fix

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
               <div className="card shadow-sm">
                 <div className="card-body row p-5">
                   <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="../assets/avatar.jpg" className="rounded-circle" alt="Enter"></img>
+                    <img src="./assets/avatar.jpg" className="rounded-circle" alt="Enter"></img>
                   </div>
                   <form onSubmit={handleSubmit} className="col-12 col-md-6 mt-3 mt-md-0">
                   <h1 className="text-center mb-4">Enter</h1>
