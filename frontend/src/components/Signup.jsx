dee6cd2c252421aca279d0b5f4c5670ecc1d9547
Tatyana N <mairon.angband@gmail.com>
2025-05-23 16:43:15 +0300

    chat

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import i18next from 'i18next';
 import avatar from '../assets/avatar-signup.jpg';
 
 const Signup = () => {
+
+    i18next.init({
+      lng: 'ru',
+      resources: {},
+    });
+
   return (
     <div className="container-fluid h-100">
       <div className="row justify-content-center align-content-center h-100">
@@ -11,25 +20,76 @@ const Signup = () => {
               <div>
                 <img src={avatar}className="rounded-circle" alt="Регистрация" />
               </div>
-              <form className="w-50">
+              <Formik
+              initialValues={{ username: '', password: '', confirmPassword: '' }}
+              validationSchema={yup.object().shape({
+                username: yup.string()
+                  .trim()
+                  .min(3, i18next.t('validate.errorNameMin'))
+                  .max(10, i18next.t('validate.errorNameMax')),
+                password: yup.string()
+                  .trim()
+                  .min(6, i18next.t('validate.errorPasswordMin'))
+                  .max(12, i18next.t('validate.errorPasswordMax')),
+                confirmPassword: yup.string()
+                  .min(6, i18next.t('validate.errorPasswordMin'))
+                  .max(12, i18next.t('validate.errorPasswordMax'))
+                  .oneOf([yup.ref('password'), null], i18next.t('validate.errorConfirmPassword'))
+                  .required(i18next.t('validate.errorRequired')),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+              }}
+              >
+                {({ isSubmitting }) => (
+              <Form className="w-50">
                 <h1 className="text-center mb-4">Регистрация</h1>
                 <div className="form-floating mb-3">
-                  <input placeholder="От 3 до 20 символов" name="username" autocomplete="username" required="" id="username" className="form-control is-invalid" value="" />
-                  <label className="form-label" for="username">Имя пользователя</label>
-                  <div placement="right" className="invalid-tooltip">Обязательное поле</div>
+                  <Field 
+                    placeholder="От 3 до 20 символов" 
+                    name="username" 
+                    autocomplete="username" 
+                    required="" 
+                    id="username" 
+                    className="form-control" 
+                    value="" 
+                  />
+                  <label className="form-label" htmlFor="username">{i18next.t('form.usernameLabel')}</label>
                 </div>
                 <div className="form-floating mb-3">
-                  <input placeholder="Не менее 6 символов" name="password" aria-describedby="passwordHelpBlock" required="" autocomplete="new-password" type="password" id="password" className="form-control" value="" />
-                  <div className="invalid-tooltip">Обязательное поле</div>
-                  <label className="form-label" for="password">Пароль</label>
+                  <Field 
+                    placeholder="Не менее 6 символов" 
+                    name="password" 
+                    type="password" 
+                    aria-describedby="passwordHelpBlock" 
+                    required="" 
+                    autocomplete="new-password" 
+                    id="password" 
+                    className="form-control" 
+                    value="" 
+                  />
+                  <ErrorMessage name="password" component="div" className="invalid-tooltip" />
+                  <label className="form-label" htmlFor="password">{i18next.t('form.passwordLabel')}</label>
                 </div>
                 <div className="form-floating mb-4">
-                  <input placeholder="Пароли должны совпадать" name="confirmPassword" required="" autocomplete="new-password" type="password" id="confirmPassword" className="form-control" value="" />
-                  <div className="invalid-tooltip"></div>
-                  <label className="form-label" for="confirmPassword">Подтвердите пароль</label>
+                  <Field 
+                    placeholder="Пароли должны совпадать" 
+                    name="confirmPassword" 
+                    type="password" 
+                    required="" 
+                    autocomplete="new-password" 
+                    id="confirmPassword" 
+                    className="form-control" 
+                    value="" 
+                  />
+                  <ErrorMessage name="confirmPassword" component="div" className="invalid-tooltip" />
+                  <label className="form-label" htmlFor="confirmPassword">{i18next.t('form.confirmPasswordLabel')}</label>
                 </div>
-                <button type="submit" className="w-100 btn btn-outline-primary">Зарегистрироваться</button>
-              </form>
+                <button type="submit" className="w-100 btn btn-outline-primary" disabled={isSubmitting}>
+                {i18next.t('buttons.submit')}
+                </button>
+              </Form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>
--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,1 +1,21 @@
-export default {};
+export default {
+    translation: {
+        form: {
+          registration: 'Регистрация',
+          usernameLabel: 'Имя пользователя',
+          passwordLabel: 'Пароль',
+          confirmPasswordLabel: 'Подтвердите пароль',
+        },
+        validate: {
+          errorNameMin: 'минимум 3 cимвола',
+          errorNameMax: 'максимум 10 символов',
+          errorPasswordMin: 'минимум 6 символов',
+          errorPasswordMax: 'максимум 12 символов',
+          errorConfirmPassword: 'Пароли должны совпадать',
+          errorRequired: 'это обязательное поле',
+        },
+        buttons: {
+          submit: 'Зарегистрироваться',
+        },
+      },
+  };
