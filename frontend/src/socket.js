7549b5ce07bf1d3ce0e55e22194996305b3b37bd
Tatyana N <mairon.angband@gmail.com>
2025-05-25 20:11:59 +0300

    in progress

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  transports: ['websocket'],
-  autoConnect: true,
-});
+const socket = io();
 
 export default socket;
