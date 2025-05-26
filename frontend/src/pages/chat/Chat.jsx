2355dc7e5debc2ed7cc55ebb736e284014a38a96
Tatyana N <mairon.angband@gmail.com>
2025-05-26 15:52:27 +0300

    f

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -21,7 +21,6 @@ const Chat = ({ channel, messages, addMessage }) => {
       channel,
     };
 
-    console.log('send message:', newMessage);
     socket.emit('sendMessage', newMessage);
     setMessageInput('');
   };
@@ -33,7 +32,7 @@ const Chat = ({ channel, messages, addMessage }) => {
           <p className="m-0"><b># {channel}</b></p>
           <span className="text-muted">{messages.length} messages</span>
         </div>
-        <div id="messages-box" ref={messagesBoxRef} className="chat-messages overflow-auto px-5">
+        <div id="messages-box" className="chat-messages overflow-auto px-5" style={{flexGrow: 1}}>
           {messages.map((msg) => (
             <div key={msg.id} className="text-break mb-2">
               <b>{msg.author}</b>: {msg.text}
@@ -50,10 +49,11 @@ const Chat = ({ channel, messages, addMessage }) => {
                 className="border-0 p-0 ps-2 form-control"
                 value={messageInput}
                 onChange={handleInputChange}
+                autoComplete="off"
               />
               <button type="submit" className="btn btn-group-vertical">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
-                  <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                  <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                 </svg>
                 <span className="visually-hidden">Отправить</span>
               </button>
