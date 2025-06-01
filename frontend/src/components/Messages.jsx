ef3538e7b30079e62040a47b7644654b53b7a53d
Tatyana N <mairon.angband@gmail.com>
2025-06-01 18:12:41 +0300

    icon

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useAuth } from '../util/useAuth.js';
 import { useGetMessagesQuery, useSendMessageMutation } from '../services/messagesApi.js';
 import { setMessages, combineMessages } from '../store/messagesSlice.js';
+import { FaSquareCaretRight } from "react-icons/fa";
 
 const Messages = () => {
  
@@ -101,9 +102,7 @@ const Messages = () => {
                 disabled={isLoading}
               />
               <button type="submit" className="btn btn-group-vertical" disabled={isLoading}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
-                  <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                </svg>
+                <FaSquareCaretRight size={20} color="currentColor" />
                 <span className="visually-hidden">Отправить</span>
               </button>
             </div>
