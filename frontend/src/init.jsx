6fb9edbf7cb0ad8951d1bdacccd1015fcce41f4a
Tatyana N <mairon.angband@gmail.com>
2025-06-03 18:25:31 +0300

    fix

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -5,6 +5,7 @@ import resources from './locales/index.js';
 import { Provider } from 'react-redux';
 import store from './store/index.js'
 import socket from './util/socket.js';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -16,12 +17,21 @@ const init = async () => {
       fallbackLng: 'ru',
     });
 
+    const rollbarConfig = {
+      accessToken: 'eea9580c95cd4eb4a736241eb270f904',
+      environment: 'testenv',
+    };
+
   return (
-    <Provider store={ store }>
-      <I18nextProvider i18n={i18n}>
-        <App socket={socket} />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider instance={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={ store }>
+          <I18nextProvider i18n={i18n}>
+            <App socket={socket} />
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
