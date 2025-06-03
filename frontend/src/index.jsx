bc8450bf48a91cb0d477b148addad38a466e9831
Tatyana N <mairon.angband@gmail.com>
2025-06-03 20:20:15 +0300

    fix

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
 const app = async () => {
+  const vdom = await init();
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
-  root.render(await init());
+  root.render(vdom);
 };
 
 app();
