2d54da5be524706d65c4e9406a26b8eabdc4bcc5
Tatyana N <mairon.angband@gmail.com>
2025-06-04 22:09:27 +0300

    fix socket

---
+++ frontend/src/util/useSocket.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addMessage } from '../store/messagesSlice.js';
+import { addChannels, removeChannels, renameChannels } from '../store/channelsSlice.js';
+import socket from '../util/socket.js';
+
+const useSocket = (refetch) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      const socketEvents = [
+        ['newMessage', (msg) => dispatch(addMessage(msg))],
+        ['newChannel', (channel) => {
+          dispatch(addChannels(channel));
+          refetch();
+        }],
+        ['removeChannel', (channel) => {
+          dispatch(removeChannels(channel.id));
+          refetch();
+        }],
+        ['renameChannel', (channel) => {
+          dispatch(renameChannels(channel));
+          refetch();
+        }],
+      ];
+  
+      socketEvents.forEach(([event, handler]) => socket.on(event, handler));
+  
+      return () => {
+        socketEvents.forEach(([event, handler]) => socket.off(event, handler));
+      };
+    }, [dispatch, refetch]);
+};
+
+  export default useSocket
