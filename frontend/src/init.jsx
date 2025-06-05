8d338ed5be5d5922c16de87875fb8d79a226c9d9
Tatyana N <mairon.angband@gmail.com>
2025-06-05 16:01:13 +0300

    fix

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -5,8 +5,8 @@ import resources from './locales/index.js';
 import { Provider } from 'react-redux';
 import store from './store/index.js'
 import socket from './util/socket.js';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-import Rollbar from 'rollbar';
+//import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+//import Rollbar from 'rollbar';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -18,24 +18,25 @@ const init = async () => {
       fallbackLng: 'ru',
     });
 
-    const token = import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN || '';
+  /*  const token = import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN;
     const rollbarInstance = new Rollbar({
       accessToken: token,
       captureUncaught: true,
       captureUnhandledRejections: true,
       environment: 'production'
     });
+    */
 
   return (
-    <RollbarProvider instance={rollbarInstance}>
-      <ErrorBoundary>
+  //  <RollbarProvider instance={rollbarInstance}>
+   //   <ErrorBoundary>
         <Provider store={ store }>
           <I18nextProvider i18n={i18n}>
             <App socket={socket} />
           </I18nextProvider>
         </Provider>
-      </ErrorBoundary>
-    </RollbarProvider>
+  //    </ErrorBoundary>
+  //  </RollbarProvider>
   );
 };
 
